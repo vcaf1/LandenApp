88449204a1e3caccd2139b24fb88a666684643c9
Casper Daris <casperdaris@msn.com>
2020-05-22 23:57:30 +0200

    De database heeft nu meer columns en er kan meer data ingeveord worden in de mainactivity

--- app/src/main/java/com/casperdaris/beroepsproductgroepc/DatabaseHelper.java
+++ app/src/main/java/com/casperdaris/beroepsproductgroepc/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.casperdaris.beroepsproductgroepc;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -19,6 +20,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String REGIO_TABLE = "REGIO_TABLE";
     public static final String COLUMN_REGIO_NAAM = "REGIO_NAAM";
     public static final String COLUMN_REGIO_BESCHRIJVING = "REGIO_BESCHRIJVING";
+    public static final String COLUMN_HOOFD_REGIO = "HOOFD_REGIO";
+    public static final String COLUMN_HOOFD_STAD = "HOOFD_STAD";
+    public static final String COLUMN_POPULATIE = "POPULATIE";
+    public static final String COLUMN_REGIO_VALUTA = "REGIO_VALUTA";
+    public static final String COLUMN_REGIO_SOORT = "REGIO_SOORT";
+    public static final String COLUMN_ALARM_NUMMER = "ALARM_NUMMER";
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, "regioDatabase", null, 1);
@@ -28,7 +35,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
 
         // Een string aanmaken om vervolgens een tabel mee te maken in de database
-        String tableMakenStatement = "CREATE TABLE " + REGIO_TABLE + " (" + COLUMN_REGIO_NAAM + " TEXT PRIMARY KEY, " + COLUMN_REGIO_BESCHRIJVING + " TEXT)";
+        String tableMakenStatement = "CREATE TABLE " + REGIO_TABLE + " (" + COLUMN_REGIO_NAAM + " TEXT PRIMARY KEY, " + COLUMN_REGIO_BESCHRIJVING + " TEXT, " + COLUMN_HOOFD_REGIO + " TEXT, " + COLUMN_HOOFD_STAD + " TEXT, " + COLUMN_POPULATIE + " INTEGER, " + COLUMN_REGIO_VALUTA + " TEXT, " + COLUMN_REGIO_SOORT + " TEXT, " + COLUMN_ALARM_NUMMER + " TEXT)";
 
         db.execSQL(tableMakenStatement);
     }
@@ -46,6 +53,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         cv.put(COLUMN_REGIO_NAAM, regio.getRegioNaam());
         cv.put(COLUMN_REGIO_BESCHRIJVING, regio.getBeschrijving());
+        cv.put(COLUMN_HOOFD_REGIO, regio.getHoofdRegio());
+        cv.put(COLUMN_HOOFD_STAD, regio.getHoofdStad());
+        cv.put(COLUMN_POPULATIE, regio.getPopulatie());
+        cv.put(COLUMN_REGIO_VALUTA, regio.getRegioValuta());
+        cv.put(COLUMN_REGIO_SOORT, regio.getRegioSoort());
+        cv.put(COLUMN_ALARM_NUMMER, regio.getAlarmNummer());
 
         // Als het toevoegen succesvol is, krijg je 1 terug, anders -1
         long insert = db.insert(REGIO_TABLE, null, cv);
@@ -58,6 +71,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     // Een lijst maken welke weergegeven kan worden in de ListView
+    // Deze lijst bevat ALLE regio's
     public List<String> regioLijstLaden() {
 
         // Een lijst maken welke wordt teruggegeven aan het einde van de methode
@@ -78,8 +92,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             do {
                 String regioNaam = cursor.getString(0);
                 String regioBeschrijving = cursor.getString(1);
+                String hoofdRegio = cursor.getString(2);
+                String hoofdStad = cursor.getString(3);
+                Integer populatie = cursor.getInt(4);
+                String regioValuta = cursor.getString(5);
+                String regioSoort = cursor.getString(6);
+                String alarmNummer = cursor.getString(7);
 
-                Regio nieuweRegio = new Regio(regioNaam, regioBeschrijving, null, null, null, null, null, null);
+                Regio nieuweRegio = new Regio(regioNaam, regioBeschrijving, hoofdRegio, hoofdStad, populatie, regioValuta, regioSoort, alarmNummer);
                 returnList.add(nieuweRegio.getRegioNaam());
 
             } while (cursor.moveToNext());
@@ -94,5 +114,32 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.close();
 
         return returnList;
+    }
+
+    public List<String> landenLijstLaden() {
+
+        List<String> returnList = new ArrayList<>();
+        String query = "SELECT * FROM " + REGIO_TABLE + " WHERE " + COLUMN_REGIO_SOORT + " = 'Land'";
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        if (cursor.moveToFirst()) {
+            do {
+                String regioNaam = cursor.getString(0);
+                String regioBeschrijving = cursor.getString(1);
+                String hoofdRegio = cursor.getString(2);
+                String hoofdStad = cursor.getString(3);
+                Integer populatie = cursor.getInt(4);
+                String regioValuta = cursor.getString(5);
+                String regioSoort = cursor.getString(6);
+                String alarmNummer = cursor.getString(7);
+                Regio nieuwLand = new Regio(regioNaam, regioBeschrijving, hoofdRegio, hoofdStad, populatie, regioValuta, regioSoort, alarmNummer);
+                returnList.add(nieuwLand.getRegioNaam());
+            } while (cursor.moveToNext());
+        } else {
+
+        }
+        cursor.close();
+        db.close();
+        return  returnList;
     }
 }
--- app/src/main/java/com/casperdaris/beroepsproductgroepc/MainActivity.java
+++ app/src/main/java/com/casperdaris/beroepsproductgroepc/MainActivity.java
@@ -5,26 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.casperdaris.beroepsproductgroepc.Objecten.Regio;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button testKnop, toevoegKnop;
-    private EditText regioNaamEditText, regioBeschrijvingEditText;
+    private EditText regioNaamEditText, regioBeschrijvingEditText, regioHoofdstadEditText, regioPopulatieEditText, regioAlarmnummerEditText;
+    private Spinner hoofdRegioSpinner, valutaSpinner, soortSpinner;
+
+    private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        databaseHelper = new DatabaseHelper(MainActivity.this);
+
         testKnop = findViewById(R.id.naarTabButton);
         toevoegKnop = findViewById(R.id.regioToevoegKnop);
         regioNaamEditText = findViewById(R.id.regioNaamEditText);
         regioBeschrijvingEditText = findViewById(R.id.regioBeschrijvingEditText);
+        hoofdRegioSpinner = findViewById(R.id.hoofdRegioSpinner);
+        regioHoofdstadEditText = findViewById(R.id.hoofdStadEditText);
+        regioPopulatieEditText = findViewById(R.id.populatieEditText);
+        valutaSpinner = findViewById(R.id.valutaSpinner);
+        soortSpinner = findViewById(R.id.soortSpinner);
+        regioAlarmnummerEditText = findViewById(R.id.alarmNummerEditText);
 
         testKnop.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +49,21 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        // De data voor de hoofdregio spinner ophalen
+        List<String> alleLanden = databaseHelper.landenLijstLaden();
+        ArrayAdapter<String> hoofdRegioSpinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alleLanden);
+        hoofdRegioSpinner.setAdapter(hoofdRegioSpinnerAdapter);
+
+        // Adapter voor de valuta spinner maken
+        ArrayAdapter<CharSequence> valutaSpinnerAdapter = ArrayAdapter.createFromResource(this, R.array.valuta, android.R.layout.simple_spinner_item);
+        valutaSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        valutaSpinner.setAdapter(valutaSpinnerAdapter);
+
+        // Adapter maken voor de regio soort spinner
+        ArrayAdapter<CharSequence> regioSoortSpinnerAdapter = ArrayAdapter.createFromResource(this, R.array.regioSoort, android.R.layout.simple_spinner_item);
+        regioSoortSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        soortSpinner.setAdapter(regioSoortSpinnerAdapter);
 
         toevoegKnop.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,14 +74,12 @@ public class MainActivity extends AppCompatActivity {
 
                 // Proberen om een nieuwe regio aan te maken, anders een foutmelding geven
                 try {
-                    regio = new Regio(regioNaamEditText.getText().toString(), regioBeschrijvingEditText.getText().toString(), null, null, null, null, null, null);
+                    regio = new Regio(regioNaamEditText.getText().toString(), regioBeschrijvingEditText.getText().toString(), hoofdRegioSpinner.getSelectedItem().toString(), regioHoofdstadEditText.getText().toString(), Integer.parseInt(regioPopulatieEditText.getText().toString()), valutaSpinner.getSelectedItem().toString(), soortSpinner.getSelectedItem().toString(), regioAlarmnummerEditText.getText().toString());
                     Toast.makeText(MainActivity.this, "Regio toevoegen gelukt", Toast.LENGTH_LONG).show();
                 } catch (Exception e) {
                     Toast.makeText(MainActivity.this, "Er is een fout opgetreden", Toast.LENGTH_LONG).show();
                     regio = new Regio("fout", "fout", null, null, null, null, null, null);
                 }
-
-                DatabaseHelper databaseHelper = new DatabaseHelper(MainActivity.this);
 
                 // De database helper gebruiken om een nieuwe regio toe te voegen aan de database
                 databaseHelper.regioToevoegen(regio);
